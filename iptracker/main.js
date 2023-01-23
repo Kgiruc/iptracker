f16f445a7e20707ea780bf2aa5d307329e157e09
Karol Giruc <kgiruc76@gmail.com>
2023-01-23 13:28:45 +0100

    fetch dataip

--- iptracker/main.js
+++ iptracker/main.js
@@ -1,12 +1,29 @@
 import "/style.css"
+
 const searchbar = document.getElementById('button');
 
+const link = "https://geo.ipify.org/api/v2/country?apiKey=at_t90B5nRqiZf6qkR6L5buzbhY0DE1C&ipAddress=8.8.8.8"
+
+
 function result() {
-    console.log("osz ty kurwa")
-    const box = document.createElement("div")
-    box.className ="inline-block bg-black w-[20vw] h-[20vh]"
-    box.innerText = "chidori"
-    document.body.appendChild(box)
+    const ipId = document.getElementById("ip");
+    const locId = document.getElementById("location");
+    const timeId = document.getElementById("time");
+    const ispId = document.getElementById("isp");
+
+    fetch(link)
+        .then((res) => res.json())
+        .then((data) => {
+                console.log(data.ip)
+                ipId.innerText = data.ip
+                locId.innerText = data.location.region + ", " + data.location.country
+                timeId.innerText = "UTC " + data.location.timezone
+                ispId.innerText = data.isp
+            }
+        )
+        .catch(error => console.log("Błąd: ", error));
+
+
 }
 
 searchbar.addEventListener("click", result)
